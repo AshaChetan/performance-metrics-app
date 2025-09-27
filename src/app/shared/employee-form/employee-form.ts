ad999025c4855285bcfc22ee1ec49e58c4a21b68
AshaChetan <ashakempaiah@gmail.com>
2025-09-27 10:03:03 +0530

    feat(shared): added EmployeeForm Component with validation

--- src/app/shared/employee-form/employee-form.ts
+++ src/app/shared/employee-form/employee-form.ts
@@ -1,6 +1,6 @@
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
@@ -11,4 +11,28 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class EmployeeForm {
 
+  employeeForm : FormGroup;
+
+  @Output() formSubmit = new EventEmitter<{
+    name:string;
+    role:string;
+    score:number
+  }>();
+
+  constructor(private fb: FormBuilder) {
+      this.employeeForm = this.fb.group({
+      name: ['', Validators.required],
+      role: ['', Validators.required],
+      score: [null, [Validators.required, Validators.min(0), Validators.max(100)]]
+    });
+  }
+  
+  submit() {
+    if (this.employeeForm.valid) {
+      this.formSubmit.emit(this.employeeForm.value);
+      this.employeeForm.reset();
+    } else {
+      this.employeeForm.markAllAsTouched();
+    }
+  }
 }
